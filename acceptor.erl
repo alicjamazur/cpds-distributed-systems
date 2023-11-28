d138108bb615e8eff7e020c0800fcb63e5175f97
LNJgit <loriennasarre@gmail.com>
2023-11-28 10:51:11 +0100

    ACCEPTOR/PROPOSER

--- acceptor.erl
+++ acceptor.erl
@@ -9,41 +9,58 @@ init(Name, PanelId) ->
   Voted = order:null(),
   Value = na,
   acceptor(Name, Promised, Voted, Value, PanelId).
-
+%We define the acceptor function right here.
 acceptor(Name, Promised, Voted, Value, PanelId) ->
   receive
     {prepare, Proposer, Round} ->
-      case order:gr(..., ...) of
+      %We need to compare the two tuples. 
+      case order:gr(Round, Promised) of
+        %If the round that we get is greater, we need inform the proposer and update values.
         true ->
-          ... ! {promise, ..., ..., ...},               
+          %We send a message to the proposer with the round which we are 
+          %promising, the last voted value, and the value (color)
+          Proposer ! {promise, Round, Voted, Value},               
       io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
-                 [Name, ..., ..., Value]),
+        %Dsiplay values in terminal
+                 [Name, Round, Voted, Value]),
           % Update gui
           Colour = case Value of na -> {0,0,0}; _ -> Value end,
           PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
-                     "Promised: " ++ io_lib:format("~p", [...]), Colour},
-          acceptor(Name, ..., Voted, Value, PanelId);
+                     "Promised: " ++ io_lib:format("~p", [Round]), Colour},
+          acceptor(Name, Round, Voted, Value, PanelId);
         false ->
-          ... ! {sorry, {prepare, ...}},
-          acceptor(Name, ..., Voted, Value, PanelId)
+          %We send sorry message with the value that we promised
+          Proposer ! {sorry, {prepare, Promised}},
+          %We call the acceptor function again and restart the process, with
+          %the same promised value that we had before
+          acceptor(Name, Promised, Voted, Value, PanelId)
       end;
+    %In case we receive an accept message from Proposer:
+    % (This happens when the proposer receives enough messages with promises from acceptos)
     {accept, Proposer, Round, Proposal} ->
-      case order:goe(..., ...) of
+      %We check if the round is greater than promised
+      case order:goe(Round, Promised) of
         true ->
-          ... ! {vote, ...},
-          case order:goe(..., ...) of
+          %We also check it the round is greater than the last previous voted.
+          Proposer ! {vote, Round},
+          case order:goe(Round , Voted) of
             true ->
+              %Log in terminal the new 
       io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
-                 [Name, Promised, ..., ...]),
+                 [Name, Promised, Round, Proposal]),
               % Update gui
-              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [...]), 
-                         "Promised: " ++ io_lib:format("~p", [Promised]), ...},
-              acceptor(Name, Promised, ..., ..., PanelId);
+              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), 
+                         "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
+              %We call the acceptor function again, with new values for the 
+              %last voted rounda and the color.
+              acceptor(Name, Promised, Round, Proposal, PanelId);
             false ->
-              acceptor(Name, Promised, ..., ..., PanelId)
+              %If the round is not the same or greater than the last voted,
+              %we just call the acceptor with the old values.
+              acceptor(Name, Promised, Voted, Value, PanelId)
           end;                            
         false ->
-          ... ! {sorry, {accept, ...}},
+          Proposer ! {sorry, {accept, Promised}},
           acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     stop ->
--- proposer.erl
+++ proposer.erl
@@ -20,33 +20,48 @@ init(Name, Proposal, Acceptors, Sleep, PanelId, Main) ->
   PanelId ! stop.
 
 round(Name, Backoff, Round, Proposal, Acceptors, PanelId) ->
+  %We start this function by printing out the current round and proposal.
   io:format("[Proposer ~w] Phase 1: round ~w proposal ~w~n", 
              [Name, Round, Proposal]),
   % Update gui
   PanelId ! {updateProp, "Round: " ++ io_lib:format("~p", [Round]), Proposal},
-  case ballot(Name, ..., ..., ..., PanelId) of
+  %If the ballot function returns an "ok" message, we update the 
+  %round and value of the proposer. If not we abort and we perform
+  %a timeout which increases every time we abort.
+  case ballot(Name, Round, Proposal, Acceptors, PanelId) of
     {ok, Value} ->
       {Value, Round};
     abort ->
       timer:sleep(rand:uniform(Backoff)),
-      Next = order:inc(...),
-      round(Name, (2*Backoff), ..., Proposal, Acceptors, PanelId)
+      Next = order:inc(Round),
+      %We increase the round if we did not receive an ok message.
+      round(Name, (2*Backoff), Next, Proposal, Acceptors, PanelId)
   end.
-
+%The vallor function is what manages the prepare and accept
+%requests. Basically the body of the paxos algorithm.
 ballot(Name, Round, Proposal, Acceptors, PanelId) ->
-  prepare(..., ...),
-  Quorum = (length(...) div 2) + 1,
+  %Send the prepare messages to all acceptors. First phase.
+  prepare(Round, Acceptors),
+  %The Quorum is the majority of votes needed to accept a value.
+  Quorum = (length(Acceptors) div 2) + 1,
   MaxVoted = order:null(),
-  case collect(..., ..., ..., ...) of
+  %WIn the case of the collect function giving an accepted
+  %message
+  case collect(Quorum, Round, MaxVoted,Proposal ) of
+    % (I am not sure if here it should be value or proposal)
     {accepted, Value} ->
       io:format("[Proposer ~w] Phase 2: round ~w proposal ~w (was ~w)~n", 
                  [Name, Round, Value, Proposal]),
       % update gui
       PanelId ! {updateProp, "Round: " ++ io_lib:format("~p", [Round]), Value},
-      accept(..., ..., ...),
-      case vote(..., ...) of
+      %Since now we have changed the value of the proposal to 
+      %the value given by the collect function. We send the accept
+      %messages informing all acceptors.
+      accept(Round, Value, Acceptors),
+      %When we reach the quorum for that round, we either arrive to a decision or abort.
+      case vote(Quorum, Round) of
         ok ->
-          {ok, ...};
+          {ok, Value};
         abort ->
           abort
       end;
@@ -54,39 +69,48 @@ ballot(Name, Round, Proposal, Acceptors, PanelId) ->
       abort
   end.
 
+%If there are no votes to collect then we indicate that we have received enough promises to proceed.
 collect(0, _, _, Proposal) ->
   {accepted, Proposal};
 collect(N, Round, MaxVoted, Proposal) ->
+  %I think in the case of receiving empty proposals we just keep collecting them.
   receive 
     {promise, Round, _, na} ->
-      collect(..., ..., ..., ...);
+      collect(N-1, Round, MaxVoted, Proposal);
     {promise, Round, Voted, Value} ->
-      case order:gr(..., ...) of
+      %We compare the voted value with the previous maximum voted value
+      case order:gr(Voted, MaxVoted) of
+        %If the voted value is greater than the maximum voted, we continue collecting with the updated voted number and the value.
         true ->
-          collect(..., ..., ..., ...);
+          collect(N-1,Round , Voted, Value);
+        %If the voted is below the maximum voted then we continue with the maximum previous vote and the original proposal.
         false ->
-          collect(..., ..., ..., ...)
+          collect(N-1, Round, MaxVoted, Proposal)
       end;
     {promise, _, _,  _} ->
       collect(N, Round, MaxVoted, Proposal);
     {sorry, {prepare, Round}} ->
-      collect(..., ..., ..., ...);
+      collect(N-1, Round, MaxVoted, Proposal);
     {sorry, _} ->
       collect(N, Round, MaxVoted, Proposal)
   after ?timeout ->
     abort
   end.
 
+%When no more votes are received it probably means we have reached a consensus.
 vote(0, _) ->
   ok;
 vote(N, Round) ->
   receive
+    %When we receive a vote we recursively call the function and then we decrease the number of votes.
     {vote, Round} ->
-      vote(..., ...);
+      vote(N-1, Round);
+    %This receive message catches the round numbers that are not from this round (previous rounds) and ignores them.
     {vote, _} ->
       vote(N, Round);
+
     {sorry, {accept, Round}} ->
-      vote(..., ...);
+      vote(N-1, Round);
     {sorry, _} ->
       vote(N, Round)
   after ?timeout ->
