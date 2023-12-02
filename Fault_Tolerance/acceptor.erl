e1979604b724bcb284b523afa6cbdee56b5f2fa9
LNJgit <loriennasarre@gmail.com>
2023-12-02 19:54:55 +0100

    Experiment V and beginning of fault tolerance

---
+++ Fault_Tolerance/acceptor.erl
@@ -0,0 +1,61 @@
+-module(acceptor).
+-export([start/2]).
+
+start(Name, PanelId) ->
+  spawn(fun() -> init(Name, PanelId) end).
+        
+init(Name, PanelId) ->
+  Promised = order:null(), 
+  Voted = order:null(),
+  Value = na,
+  paxy:crash(homer),
+  pers:open(Name),
+  pers:store(Name,Promised,Voted,Value,PanelId),
+  pers:close(Name),
+  acceptor(Name, Promised, Voted, Value, PanelId).
+
+acceptor(Name, Promised, Voted, Value, PanelId) ->
+  receive
+    {prepare, Proposer, Round} ->
+      case order:gr(Round, Promised) of
+        true ->
+          pers:open(Name),
+          pers:store(Name, Round, Voted, Value, PanelId),
+          pers:close(Name),
+          Proposer ! {promise, Round, Voted, Value},               
+          io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
+                   [Name, Round, Voted, Value]),
+          Colour = case Value of na -> {0,0,0}; _ -> Value end,
+          PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
+                     "Promised: " ++ io_lib:format("~p", [Round]), Colour},
+          acceptor(Name, Round, Voted, Value, PanelId);
+        false ->
+          Proposer ! {sorry, {prepare, Round}},
+          acceptor(Name, Promised, Voted, Value, PanelId)
+      end;
+    {accept, Proposer, Round, Proposal} ->
+      case order:goe(Round, Promised) of
+        true ->
+          Proposer ! {vote, Round},
+          case order:goe(Round , Voted) of
+            true ->
+              pers:open(Name),
+              pers:store(Name, Promised, Round, Proposal, PanelId),
+              pers:close(Name),
+              io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
+                         [Name, Promised, Round, Proposal]),
+              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), 
+                         "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
+              acceptor(Name, Promised, Round, Proposal, PanelId);
+            false ->
+              acceptor(Name, Promised, Voted, Value, PanelId)
+          end;                            
+        false ->
+          Proposer ! {sorry, {accept, Round}},
+          acceptor(Name, Promised, Voted, Value, PanelId)
+      end;
+    stop ->
+      pers:delete(Name),
+      PanelId ! stop,
+      ok
+  end.
---
+++ Part1/paxyEx5.erl
@@ -0,0 +1,86 @@
+-module(paxyEx5).
+-export([start/1, stop/0, stop/1]).
+
+-define(RED, {255,0,0}).
+-define(BLUE, {0,0,255}).
+-define(GREEN, {0,255,0}).
+
+% Sleep is a list with the initial sleep time for each proposer
+start(Sleep) ->
+  AcceptorNode = 'paxy-acc@127.0.0.1', % Node where acceptors will run
+  ProposerNode = 'paxy-pro@127.0.0.1', % Node where proposers will run
+  AcceptorNames = ["Homer", "Marge", "Bart", "Lisa", "Maggie"],
+  AccRegister = [homer, marge, bart, lisa, maggie],
+  DistributedAccRegister = [{homer,AcceptorNode}, {marge,AcceptorNode}, {bart,AcceptorNode}, {lisa,AcceptorNode}, {maggie,AcceptorNode}],
+  ProposerNames = [{"Fry", ?RED}, {"Bender", ?GREEN}, {"Leela", ?BLUE}],
+  PropInfo = [{fry, ?RED}, {bender, ?GREEN}, {leela, ?BLUE}],
+  register(gui, spawn(fun() -> gui:start(AcceptorNames, ProposerNames) end)),
+  gui ! {reqState, self()},
+  receive
+    {reqState, State} ->
+      {AccIds, PropIds} = State,
+      spawn(AcceptorNode, fun()-> start_acceptors(AccIds, AccRegister)end),
+      spawn(fun() -> 
+        Begin = erlang:monotonic_time(),
+        spawn(ProposerNode,fun() -> start_proposers(PropIds, PropInfo, DistributedAccRegister, Sleep, self()) end),
+        wait_proposers(length(PropIds)),
+        End = erlang:monotonic_time(),
+        Elapsed = erlang:convert_time_unit(End-Begin, native, millisecond),
+        io:format("[Paxy] Total elapsed time: ~w ms~n", [Elapsed])
+      end)
+  end.
+    
+start_acceptors(AccIds, AccReg) ->
+  case AccIds of
+    [] ->
+      ok;
+    [AccId|Rest] ->
+      [RegName|RegNameRest] = AccReg,
+      register(RegName, acceptorEx5:start(RegName, AccId)),
+      start_acceptors(Rest, RegNameRest)
+  end.
+
+start_proposers(PropIds, PropInfo, Acceptors, Sleep, Main) ->
+  case PropIds of
+    [] ->
+      ok;
+    [PropId|Rest] ->
+      [{RegName, Colour}|RestInfo] = PropInfo,
+      [FirstSleep|RestSleep] = Sleep,
+      proposerEx5:start(RegName, Colour, Acceptors, FirstSleep, PropId, Main),	
+      start_proposers(Rest, RestInfo, Acceptors, RestSleep, Main)
+  end.
+
+
+wait_proposers(0) ->
+  ok;
+wait_proposers(N) ->
+  receive
+    done ->
+      wait_proposers(N-1)
+  end.
+
+stop() ->
+  AcceptorNode = 'paxy-acc@127.0.0.1',
+  stop({homer, AcceptorNode}),
+  stop({marge, AcceptorNode}),
+  stop({bart, AcceptorNode}),
+  stop({lisa, AcceptorNode}),
+  stop({maggie, AcceptorNode}),
+  stop(gui), % Assuming GUI is local
+  ok.
+stop(Name) ->
+  case Name of
+    {RegName, Node} when is_atom(RegName), is_atom(Node) ->
+      rpc:call(Node, erlang, whereis, [RegName]),
+      rpc:call(Node, erlang, send, [RegName, stop]);
+    _ ->
+      case whereis(Name) of
+        undefined ->
+          ok;
+        Pid ->
+          Pid ! stop
+      end
+  end.
+
+ 
