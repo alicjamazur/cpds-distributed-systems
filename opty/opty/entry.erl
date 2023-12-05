e266ca19a538c08f2e9dcf800c236d94e717ed8c
eren <erenha1@outlook.com>
2023-12-05 17:28:16 +0100

    complete code

--- opty/opty/entry.erl
+++ opty/opty/entry.erl
@@ -10,15 +10,15 @@ init(Value) ->
 entry(Value, Time) ->
     receive
         {read, Ref, From} ->
-            %% TODO: ADD SOME CODE
+            From ! {Ref, self(), Value, Time},
             entry(Value, Time);
         {write, New} ->
-            entry(... , make_ref());  %% TODO: COMPLETE
+            entry(New , make_ref());
         {check, Ref, Readtime, From} ->
             if 
-                 ... == ... ->   %% TODO: COMPLETE
-                    %% TODO: ADD SOME CODE
-                true ->
+                 Readtime == Time ->
+                    From ! {Ref, ok};
+                true -> 
                     From ! {Ref, abort}
             end,
             entry(Value, Time);
